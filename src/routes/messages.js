e72b0a52a2cecdc7cba16ea3f451a2934e12ad98
MikeGonzaBar <is722298@iteso.mx>
2021-10-19 23:26:45 -0500

    added logged in filter

--- src/routes/messages.js
+++ src/routes/messages.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const val = require("../middlewares/validations.js");
 const messagesController = require('../controllers/messages.controller.js');
 
-router.post('/:channelId', messagesController.createMessage);
-router.get('/:channelId', val.validateToken,messagesController.getMessagesByChannel);
+router.post('/:channelId', val.validateToken,val.isLoggedIn, messagesController.createMessage);
+router.get('/:channelId', val.validateToken,val.isLoggedIn,messagesController.getMessagesByChannel);
 
 module.exports = router;
